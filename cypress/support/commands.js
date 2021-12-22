3df5f81392979986cbe99cf005265779b2cd9e8d
Aleksey <hoock57@gmail.com>
2021-12-22 19:19:55 +0300

    login fuction added to comand.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//login user to zero webappsecurity app
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('#user_login').type(username)
+  cy.get('#user_password').type(password)
+  cy.contains('Sign in').click()
+})
